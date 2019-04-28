ed863127287f1a7f812b86867aadb6b85d6b0a7a
raghavddps2 <raghav.ddps2@gmail.com>
2019-04-28 14:23:48 +0530

    Added Comments

--- DynamicProgramming/warshall.c
+++ DynamicProgramming/warshall.c
@@ -1,7 +1,18 @@
+/*
+    @Author: Raghav Maheshwari
+    @Date: 28th April, 2019
+    @Problem: Warshall Algorithm (Greedy Approach)
+
+    1 represents a path present whereas 0 represents there is no path present.
+    Time complexity: O(n^3)
+    Space Complexity: O(1) //If no new array is created
+                      O(n^2) //If a new array is created.
+*/
 #include<stdio.h>
 int arr[10][10] = {0};
 
-
+//Logic for warshalls. 
+//Either a direct path or (a connecting path (&& both should be one))
 void warshall(int n){
     for(int k=0;k<n;k++){
         for(int i=0;i<n;i++){
@@ -23,7 +34,7 @@ void main(){
         }
     }
 
-    warshall(n);
+    warshall(n); //Just calling the function.
 
     printf("The transitive closure of the matrix is :\n");
     for(int i=0;i<n;i++){
--- DynamicProgramming/warshall2.c
+++ DynamicProgramming/warshall2.c
@@ -1,6 +1,19 @@
+/*
+    @Author: Raghav Maheshwari
+    @Date: 28th April, 2019
+    @Problem: Warshall Algorithm (Greedy Approach)
+
+    1 represents a path present whereas 0 represents there is no path present.
+    Time complexity: O(n^3)
+    Space Complexity: O(1) //If no new array is created
+                      O(n^2) //If a new array is created.
+*/
+
 #include<stdio.h>
 
 void warshall(int n,int arr[n][n]){
+    //Logic for warshalls. 
+    //Either a direct path or (a connecting path (&& both should be one))
     for(int k=0;k<n;k++){
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
@@ -26,7 +39,7 @@ void main(){
         }
     }
 
-    warshall(n,arr);
+    warshall(n,arr); //Calling the function
 
     printf("The transitive closure is:\n");
     for(int i=0;i<n;i++){
